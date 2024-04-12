aa754c80925058162b4c27d84511d5e75dd94fb3
Smateogarzon <mateogarzon1002@gmail.com>
2024-04-12 14:49:13 -0500

    feat: :sparkles: configuracion de cors,helmet,morgan y mas

--- back-infinity/src/main.ts
+++ back-infinity/src/main.ts
@@ -1,9 +1,64 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+    logger: ['error', 'warn', 'debug', 'verbose'],
+  });
+  app.use(morgan('dev'));
+  // app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Content-Type-application/json',
+      'Authorization',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+    ],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  // app.use(cookieParser());
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            "'self'",
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+            'res.cloudinary.com',
+            '*.googleusercontent.com',
+          ],
+          scriptSrc: [
+            "'self'",
+            "https: 'unsafe-inline'",
+            'https://cdn.jsdelivr.net/npm/@apollographql/client@3.6.6/dist/bundle.js',
+          ],
+          manifestSrc: [
+            "'self'",
+            'apollo-server-landing-page.cdn.apollographql.com',
+            'res.cloudinary.com',
+          ],
+          frameSrc: ["'self'", 'sandbox.embed.apollographql.com'],
+        },
+      },
+    })
+  );
+
+  app.use(cookieParser());
+
   await app.listen(3000);
+
   console.log('http://localhost:3000');
 }
 bootstrap();
